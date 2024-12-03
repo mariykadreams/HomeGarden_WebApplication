8674716ab384567e9900437ef38be9059b28f648
mariia tokhtamysh <mariyka.tokhtamysh@gmail.com>
2024-12-03 15:59:05 +0200

    String Extensions(Highlight Text)

---
+++ KursovaHomeGarden/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using System.Web;
+
+namespace KursovaHomeGarden.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string HighlightText(this string text, string searchTerm)
+        {
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(searchTerm))
+                return HttpUtility.HtmlEncode(text);
+
+            var pattern = Regex.Escape(searchTerm);
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            return regex.Replace(
+                HttpUtility.HtmlEncode(text),
+                match => $"<span class='highlight'>{match.Value}</span>"
+            );
+        }
+    }
+}
